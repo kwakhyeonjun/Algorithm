113b15032b61185d66b17f526d7dba2c9376a331
Hyeonjun <37528765+kwakhyeonjun@users.noreply.github.com>
2021-07-14 18:12:13 +0900

    잔디심기(1일차)
    잔디심기(1일차)

---
+++ Baekjoon/src/level02/B2884.java
@@ -0,0 +1,22 @@
+package level02;
+
+import java.util.Scanner;
+
+public class B2884 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+
+        if(min < 45) {
+            min = 60 - 45 + min;
+            hour--;
+        }
+        else min -= 45;
+
+        if(hour <0) hour = 24 + hour;
+
+        System.out.println(hour + " " + min);
+    }
+}
