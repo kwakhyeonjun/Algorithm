dc9a0edcb3b0a6173c9edfa460ca1f1c7ff85aa8
Hyeonjun <37528765+kwakhyeonjun@users.noreply.github.com>
2021-07-16 20:17:27 +0900

    잔디심기(3일차) level04. while문 완료

---
+++ Baekjoon/src/level05/B10952.java
@@ -0,0 +1,17 @@
+package level05;
+
+import java.util.Scanner;
+
+public class B10952 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A, B;
+
+        do{
+            A = sc.nextInt();
+            B = sc.nextInt();
+            if(A!=0 && B!=0) System.out.println(A+B);
+        }while(A !=0 && B != 0);
+    }
+}
