8a6c415ff3ffb3001e2dc36e3c6f03e914dbe7d9
곽현준 <kwak0568@naver.com>
2021-07-21 20:47:46 +0900

    잔디심기(8일차) level07. 문자열 푸는중

--- src/Baekjoon/level07/B2941.java
+++ src/Baekjoon/level07/B2941.java
@@ -13,29 +13,30 @@ public class B2941 {
 
     private static int parse(char[] arr) {
         int result = 0;
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] == 'c' && (arr[i+1] == '=' || arr[i+1] == '-')
-                    || (arr[i] == 'l' && arr[i+1] == 'j')
-                    || (arr[i] == 'n' && arr[i+1] == 'j')
-                    || (arr[i] == 's' && arr[i+1] == '=')
-                    || (arr[i] == 'z' && arr[i+1] == '='))
-            {
-                if(i==arr.length-2) result++;
-                result++;
-                i++;
-            }else if(arr[i] == 'd') {
-                if (i < arr.length - 2 && arr[i + 1] == 'z' && arr[i + 2] == '=') {
-                    if (i == arr.length - 3) result++;
-                    result++;
-                    i += 2;
-                } else if (arr[i + 1] == '-') {
-                    if(i==arr.length-2) result++;
+        for(int i = 0; i < arr.length; i++){
+            if(i < arr.length - 1){
+                if(arr[i] == 'c' && (arr[i+1] == '=' || arr[i+1] == '-')
+                        || (arr[i] == 'l' && arr[i+1] == 'j')
+                        || (arr[i] == 'n' && arr[i+1] == 'j')
+                        || (arr[i] == 's' && arr[i+1] == '=')
+                        || (arr[i] == 'z' && arr[i+1] == '='))
+                {
                     result++;
                     i++;
+                }else if(arr[i] == 'd') {
+                    if (i < arr.length - 2 && arr[i + 1] == 'z' && arr[i + 2] == '=') {
+                        result++;
+                        i += 2;
+                    } else if (arr[i + 1] == '-') {
+                        result++;
+                        i++;
+                    } else {
+                        result++;
+                    }
+                }else{
+                    result++;
                 }
-            }
-            else{
-                if(i == arr.length - 2) result++;
+            }else{
                 result++;
             }
         }
