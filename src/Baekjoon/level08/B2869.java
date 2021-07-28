fe1539c3ee5f0b8e968ad53414a8f43a98313ab7
곽현준 <kwak0568@naver.com>
2021-07-28 21:30:02 +0900

    잔디심기(14일차)

---
+++ src/Baekjoon/level08/B2869.java
@@ -0,0 +1,27 @@
+package Baekjoon.level08;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B2869 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[3];
+        try{
+            String[] input = br.readLine().split(" ");
+            for(int i = 0; i < 3; i++) {
+                arr[i] = Integer.parseInt(input[i]);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        int A = arr[0];
+        int B = arr[1];
+        int V = arr[2];
+        int day = (V - B)/(A - B);
+        if((V-B)%(A-B) != 0) day++;
+        System.out.println(day);
+    }
+}
--- src/SWEA/D2/SWEA1940.java
+++ src/SWEA/D2/SWEA1940.java
@@ -6,6 +6,6 @@ public class SWEA1940 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt()
+        int N = sc.nextInt();
     }
 }
