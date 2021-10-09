757318030f829a9748d95999c6ffa5b21f0afeb1
곽현준 <kwak0568@naver.com>
2021-10-09 20:26:38 +0900

    잔디심기

---
+++ src/Baekjoon/Grade/Silver/B1931.java
@@ -0,0 +1,40 @@
+package Baekjoon.Grade.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class B1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] rooms = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            rooms[i] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        }
+
+        Arrays.sort(rooms, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            int start = rooms[i][0];
+            int end = rooms[i][1];
+
+            System.out.println(start + " " + end);
+        }
+    }
+}
