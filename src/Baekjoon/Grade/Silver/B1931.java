9e91144c5e020a5521f8ce56331d90a1245bea9f
곽현준 <kwak0568@naver.com>
2021-10-10 21:23:35 +0900

    잔디심기

--- src/Baekjoon/Grade/Silver/B1931.java
+++ src/Baekjoon/Grade/Silver/B1931.java
@@ -34,7 +34,7 @@ public class B1931 {
             int start = rooms[i][0];
             int end = rooms[i][1];
 
-            System.out.println(start + " " + end);
+            if(start )
         }
     }
 }
