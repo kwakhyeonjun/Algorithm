c6291054c0536e7a7bb71adb761818c187379e27
곽현준 <kwak0568@naver.com>
2021-10-24 20:11:55 +0900

    잔디심기

---
+++ src/Baekjoon/Grade/Silver/B1541.java
@@ -0,0 +1,33 @@
+package Baekjoon.Grade.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1541 {
+    private static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        char operand = '0';
+        answer = 0;
+        boolean minus = false;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) >= '0' && s.charAt(i) <= '9') sb.append(s.charAt(i));
+            else{
+                if(operand == '0') answer += Integer.parseInt(sb.toString());
+                if(operand == '-') minus = true;
+                operand = s.charAt(i);
+                if(minus) answer -= Integer.parseInt(sb.toString());
+                sb = new StringBuilder();
+            }
+        }
+        if(minus) answer -= Integer.parseInt(sb.toString());
+        else answer += Integer.parseInt(sb.toString());
+        System.out.println(answer);
+        br.close();
+    }
+}
