d6654d2d4ebb3193e26164b9c7598eb33320eda5
곽현준 <kwak0568@naver.com>
2021-11-04 20:05:33 +0900

    잔디심기

--- src/Baekjoon/Grade/Silver/B2217.java
+++ src/Baekjoon/Grade/Silver/B2217.java
@@ -1,7 +1,9 @@
 package Baekjoon.Grade.Silver;
 
+import java.util.Scanner;
+
 public class B2217 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
