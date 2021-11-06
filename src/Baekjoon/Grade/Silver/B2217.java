5734564fd170a479295f75880aaaf46f01fabdb1
곽현준 <kwak0568@naver.com>
2021-11-06 20:12:43 +0900

    잔디심기

--- src/Baekjoon/Grade/Silver/B2217.java
+++ src/Baekjoon/Grade/Silver/B2217.java
@@ -1,10 +1,22 @@
 package Baekjoon.Grade.Silver;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class B2217 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int[] rope = new int[N];
+        for (int i = 0; i < N; i++) {
+            rope[i] = sc.nextInt();
+        }
+        Arrays.sort(rope);
+        long answer = 0;
+        for(int i = N - 1; i >= 0; i--){
+            rope[i] = rope[i] * (N - i);
+            answer = Math.max(answer, rope[i]);
+        }
+        System.out.println(answer);
     }
 }
