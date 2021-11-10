02321ec87fe13b7be2674add796e6274b4a2bfa8
곽현준 <kwak0568@naver.com>
2021-11-10 20:30:23 +0900

    잔디심기

--- src/Baekjoon/Grade/Gold/B1339.java
+++ src/Baekjoon/Grade/Gold/B1339.java
@@ -1,7 +1,26 @@
 package Baekjoon.Grade.Gold;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class B1339 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[26];
+        String[] input = new String[N];
+        for(int i = 0; i < N; i++){
+            input[i] = br.readLine();
+        }
+        Arrays.sort(input, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length() - o1.length();
+            }
+        });
 
     }
 }
