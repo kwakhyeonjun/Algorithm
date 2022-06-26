a0a285e76942ba15982d284c6ef7deb912f7debf
kwakhyeonjun <kwak0568@naver.com>
2022-06-26 15:54:56 +0900

    잔디심기

--- src/Programmers/SkillCheck/SkillCheck2_1_1.java
+++ src/Programmers/SkillCheck/SkillCheck2_1_1.java
@@ -1,41 +1,58 @@
 package Programmers.SkillCheck;
 
-import java.util.Arrays;
-
 public class SkillCheck2_1_1 {
     public static void main(String[] args) {
         SkillCheck2_1_1 sol = new SkillCheck2_1_1();
-        int[] numbers = {3, 30, 34, 5, 9};
-        System.out.println(sol.solution(numbers));
+        int n = 16;
+        int t = 16;
+        int m = 2;
+        int p = 1;
+        System.out.println(sol.solution(n, t, m, p));
     }
 
-    public String solution(int[] numbers) {
+    public String solution(int n, int t, int m, int p) {
         StringBuilder answer = new StringBuilder();
-
-        String[] nums = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            nums[i] = String.valueOf(numbers[i]);
+        StringBuilder call = new StringBuilder();
+        int cur = 0;
+        while(call.length() <= t * m + p) {
+            call.append(getDigit(n, cur++));
         }
-        Arrays.sort(nums);
 
-        Arrays.sort(nums, (o1, o2) -> {
-            int idx1 = 0, idx2 = 0;
-            while(true) {
-                if(idx1 == o1.length()) return 1;
-                if(idx2 == o2.length()) return -1;
-                if(o1.charAt(idx1) == o2.charAt(idx2)) {
-                    idx1++;
-                    idx2++;
-                    continue;
-                }
-                return o2.charAt(idx2) - o1.charAt(idx1);
-            }
-        });
-
-        for(String num : nums) answer.append(num);
+        for (int i = p - 1; i < t * m; i += m) {
+            answer.append(call.charAt(i));
+        }
 
         return answer.toString();
     }
 
-
+    private String getDigit(int n, int num) {
+        StringBuilder sb = new StringBuilder();
+        while(num > 0) {
+            switch (num % n) {
+                case 10:
+                    sb.append("A");
+                    break;
+                case 11:
+                    sb.append("B");
+                    break;
+                case 12:
+                    sb.append("C");
+                    break;
+                case 13:
+                    sb.append("D");
+                    break;
+                case 14:
+                    sb.append("E");
+                    break;
+                case 15:
+                    sb.append("F");
+                    break;
+                default:
+                    sb.append(num%n);
+            }
+            num /= n;
+        }
+        if(sb.toString().equals("")) return "0";
+        return sb.reverse().toString();
+    }
 }
--- src/Programmers/SkillCheck/SkillCheck2_2_1.java
+++ src/Programmers/SkillCheck/SkillCheck2_2_1.java
@@ -3,45 +3,80 @@ package Programmers.SkillCheck;
 
 public class SkillCheck2_2_1 {
     public static void main(String[] args) {
-        System.out.println(solution("ababcdcdababcdcd"));
+        SkillCheck2_2_1 sol = new SkillCheck2_2_1();
+        String s = "ababcdcdababcdcd";
+        System.out.println(sol.solution(s));
     }
-    public static int solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        String temp = "";
-        int min = Integer.MAX_VALUE;
 
-        for(int t = 1; t < s.length()/2; t++){
-            int idx = 0;
-            while(idx < s.length()){
-                boolean isFind = false;
-                int count = 0;
-                if(idx+t < s.length()){
-                    temp = s.substring(idx, idx + t);
-                    if(s.charAt(idx) == s.charAt(idx+t)){
-                        count = 1;
-                        for(int j = idx+t; j < s.length(); j += t){
-                            if(temp.equals(s.substring(j, j+t))){
-                                isFind = true;
-                                count++;
-                            }else{
-                                idx = j;
-                                break;
-                            }
-                            if(j == s.length() - t) idx = j + t;
-                        }
+    public int solution(String s) {
+        int answer = s.length();
+
+        // 압축할 개수
+        for (int t = 1; t <= s.length() / 2; t++) {
+            StringBuilder sb = new StringBuilder();
+            char cur;
+            String prev = "";
+            int count = 1;
+            for (int i = 0; i < s.length(); i++) {
+                if(i + 2 * t > s.length()) {
+                    if(count > 1) {
+                        sb.append(count);
+                        sb.append(prev);
+                    }else{
+                        sb.append(s, i, s.length());
                     }
+                    break;
                 }
-                if(isFind){
-                    sb.append(count + temp);
-                }else{
-                    sb.append(s.charAt(idx));
-                    idx += t;
+                cur = s.charAt(i);
+                if(s.substring(i, i + t).equals(s.substring(i + t, i + 2 * t))){
+                    prev = s.substring(i, i + t);
+                    i += t - 1;
+                    count++;
+                } else {
+                    if(count == 1) sb.append(cur);
+                    else {
+                        sb.append(count);
+                        sb.append(prev);
+                        i += t - 1;
+                    }
+                    count = 1;
+                }
+                if(i == 0 && count == 1) {
+                    sb.append(s);
+                    break;
                 }
             }
-            min = Math.min(min, sb.length());
-            System.out.println(sb);
-            sb.delete(0, sb.length());
+            answer = Math.min(answer, sb.length());
         }
-        return min;
+
+        return answer;
+    }
+
+    public int solution2(String s) {
+        int answer = s.length();    // 압축하기 전 문자열 길이로 초기화
+
+        for(int i = 1; i <= s.length() / 2; i++){
+            int zipLevel = 1;                           // 현재 압축정도
+            String zipStr = s.substring(0, i);          // 압축할 문자
+            StringBuilder result = new StringBuilder(); // 압축완료한 문자를 저장할 StringBuilder
+
+            for(int j = i; j <= s.length(); j += i){
+                // 다음 문자 추출
+                String next = s.substring(j, Math.min(j + i, s.length()));
+                // 다음 문자와 현재 문자가 같으면 zipLevel증가
+                if(zipStr.equals(next)) zipLevel++;
+                    // 다음 문자와 현재 문자가 다를 경우
+                else{
+                    // (압축이 안됬을 경우는 공백, 압축이 됬을경우 zipLevel을 붙여줌) + 압축할 문자를 넣어줌,
+                    result.append(zipLevel != 1 ? zipLevel : "").append(zipStr);
+                    zipStr = next;      // 다음 문자를 압축할 문자로 지정
+                    zipLevel = 1;       // 압축 정도 1로 초기화
+                }
+            }
+            result.append(zipStr);      // 마지막에 추가안된 zipStr추가
+            answer = Math.min(answer, result.length()); // 가장 작은 문자열 길이 저장
+        }
+
+        return answer;
     }
 }
