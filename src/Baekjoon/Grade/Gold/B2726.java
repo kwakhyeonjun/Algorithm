5418e4c1b0d29972d087b430174b00a792ac800a
KwakHyeonjun <kwak0568@naver.com>
2022-08-05 17:51:52 +0900

    잔디심기

--- src/Baekjoon/Grade/Gold/B2726.java
+++ src/Baekjoon/Grade/Gold/B2726.java
@@ -5,6 +5,9 @@ import AlgoStudy.Week05.map;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class B2726 {
     public static void main(String[] args) throws IOException {
@@ -14,36 +17,50 @@ public class B2726 {
         for(int t = 0; t < tc; t++) {
             n = Integer.parseInt(br.readLine());
             map = new boolean[n][n];
+            stack = new Stack<>();
 
-
-        }
-    }
-
-    private static int n;
-    private static boolean[][] map;
-
-    private static void setChess(boolean[][] map, int count){
-        if(count == map.length) {
-
-            return;
-        }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j <= i; j++) {
-                if(map[i][j]) continue;
-                boolean[][] copyMap = copyMap(map);
-                map[i][j] = true;
-                setMap(map, i, j);
+            setChess(map, 0);
+            System.out.println(max);
+            for(int[] cur : stack) {
+                System.out.println(cur[0] + " " + cur[1]);
             }
         }
     }
 
-    private static void setMap(boolean[][] map, int x, int y) {
-        // 가로
-        for(int i = 0; i < x; i++) {
-            map[x][i] = true;
+    private static int n, max;
+    private static boolean[][] map;
+    private static Stack<int[]> stack;
+
+    private static void setChess(boolean[][] map, int count){
+        for(int i = count; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                if(!isPos(map, i, j)) continue;
+                map[i][j] = true;
+                stack.add(new int[]{i, j});
+                setChess(map, count);
+                map[i][j] = false;
+                stack.pop();
+            }
         }
+        max = Math.max(count, max);
     }
 
+    private static boolean isPos(boolean[][] map, int x, int y) {
+        // 가로
+        for(int i = 0; i < x; i++) {
+            if(map[x][i]) return false;
+        }
+
+        for(int i = y; i < n; i++) {
+            if(map[i][y]) return false;
+        }
+
+        for(int i = 0, j = 0; i < n || j < n; i++, j++) {
+            if(map[i][j]) return false;
+        }
+
+        return true;
+    }
     private static boolean[][] copyMap(boolean[][] map) {
         boolean[][] copyMap = new boolean[n][n];
         for(int i = 0; i < n; i++) {
