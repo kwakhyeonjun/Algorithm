0ee534f6929c45832527659825cdceca19ef448e
KwakHyeonjun <kwak0568@naver.com>
2023-04-03 08:15:51 +0900

    잔디심기

---
+++ src/Programmers/Level2/P169198.java
@@ -0,0 +1,12 @@
+package Programmers.Level2;
+
+public class P169198 {
+
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+
+        
+
+        return answer;
+    }
+}
