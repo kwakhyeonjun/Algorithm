0e2a1f1030e69f5ab10318fc8f1ad282ffdf9df1
KwakHyeonjun <kwak0568@naver.com>
2023-07-20 09:34:52 +0900

    알고리즘 문제풀이 Programmers 12985 - 예상 대진표

---
+++ src/Programmers/ret/P12985.java
@@ -0,0 +1,14 @@
+package Programmers.ret;
+
+public class P12985 {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+        while(a != b) {
+            answer++;
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+        }
+
+        return answer;
+    }
+}
