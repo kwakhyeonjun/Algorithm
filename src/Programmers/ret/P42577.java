6619d0623a0d20b259e7daef58245c7d20a4de60
KwakHyeonjun <kwak0568@naver.com>
2023-08-04 12:00:40 +0900

    알고리즘 문제풀이 Programmers 42577 - 전화번호 목록

---
+++ src/Programmers/ret/P42577.java
@@ -0,0 +1,16 @@
+package Programmers.ret;
+
+import java.util.Arrays;
+
+public class P42577 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i].length() < phone_book[i + 1].length()) {
+                if(phone_book[i + 1].startsWith(phone_book[i])) return false;
+            }
+        }
+        return true;
+    }
+}
